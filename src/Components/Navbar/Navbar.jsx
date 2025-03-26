aafba6ad8a002bcb41cb94ff05cb515fa54b0ea2
soumya9567 <soumyaammu1917@gmail.com>
2025-03-27 02:57:02 +0530

    navbar cmplt

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -30,10 +30,10 @@ function Navbar() {
                 </div>
 
                <div className={`absolute md:relative top-16 md:top-0 left-0 w-full md:w-auto bg-black md:bg-transparent flex flex-col md:flex-row items-center md:space-x-6 text-lg transition-all duration-300 ${expand ? "block" : "hidden"} md:flex`}>
-                    <div className="hover:text-pink-500 transition duration-300 cursor-pointer p-3 md:p-0">HOME</div>
-                    <div className="hover:text-pink-500 transition duration-300 cursor-pointer p-3 md:p-0">ABOUT</div>
-                    <div className="hover:text-pink-500 transition duration-300 cursor-pointer p-3 md:p-0">CONTACT</div>
-                    <div className="hover:text-pink-500 transition duration-300 cursor-pointer p-3 md:p-0">PROJECT</div>
+                    <div className="hover:text-pink-500  cursor-pointer p-3 md:p-0">HOME</div>
+                    <div className="hover:text-pink-500  cursor-pointer p-3 md:p-0">ABOUT</div>
+                    <div className="hover:text-pink-500  cursor-pointer p-3 md:p-0">CONTACT</div>
+                    <div className="hover:text-pink-500  cursor-pointer p-3 md:p-0">PROJECT</div>
 
                     <button className="mt-4 md:mt-0 bg-blue-500 w-28 h-9 rounded-lg hover:bg-pink-600 transition duration-300">
                         RESUME
